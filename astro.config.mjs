3b90aa8dc3d604247e159e58bc06483aa910ac0b
Hamid Ranjbar <123825536+ranjbar-hamid@users.noreply.github.com>
2025-02-21 15:33:11 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
-// import netlify from "@astrojs/netlify";
-
-// import node from "@astrojs/node";
-
-import vercel from "@astrojs/vercel";
-
-// https://astro.build/config
+import node from "@astrojs/node";
 export default defineConfig({
   output: "server",
-
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
