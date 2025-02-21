6f80a2410265ac6f9940e01b3a3cd7139f3279ee
Hamid Ranjbar <123825536+ranjbar-hamid@users.noreply.github.com>
2025-02-21 16:05:22 +0330

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// import netlify from "@astrojs/netlify";
+
+import node from "@astrojs/node";
+
+// import vercel from "@astrojs/vercel";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: node({
+    mode: "middleware",
+  }),
+});
