33b6fe550c3467c6837ec49d28610fa3f11d5fa7
Hamid Ranjbar <123825536+ranjbar-hamid@users.noreply.github.com>
2025-02-21 13:58:41 +0330

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  
+  site: "http://example.com",
   output: "server",
   adapter: netlify(),
   vite: {
