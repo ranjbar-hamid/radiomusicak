21ccc561bdd37774cf4df157e40dbc8bbbd499bc
Hamid Ranjbar <123825536+ranjbar-hamid@users.noreply.github.com>
2025-03-28 14:22:39 +0330

    Add files via upload

---
+++ src/pages/pic/[id].js
@@ -0,0 +1,27 @@
+export async function GET({ params, request }) {
+  try {
+    const f = await fetch(
+      `https://script.google.com/macros/s/AKfycbz2ilrGauIAJoWzpZ3IcBv6gEato1hUWcC5p-1OJctJZcmwSIiLvrA5n5c8QZ9E5Mb3/exec?t=pic&id=${params.id}`,
+      { cache: "reload" }
+    );
+
+    const js = f.json();
+
+    return new Response(
+      await js.then(
+        (i) => {
+          const base64Data = i.pic.split(",")[1] || i.pic;
+          const binaryData = Buffer.from(base64Data, "base64");
+          return binaryData;
+        },
+        {
+          headers: {
+            "Content-Type": "image/jpeg",
+          },
+        }
+      )
+    );
+  } catch (e) {
+    return new Response("");
+  }
+}
